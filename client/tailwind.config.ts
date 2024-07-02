3a75a02946ba482a0f9cdbed601fb8e3616b82b8
Serhii Illarionov <sergio.illarionov@gmail.com>
2024-07-02 20:54:18 +0300

    initial commit

---
+++ client/tailwind.config.ts
@@ -0,0 +1,65 @@
+import type { Config } from "tailwindcss"
+
+const config = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  prefix: "",
+  theme: {
+    screens: {
+      "xs": "480px",
+      "sm": "640px",
+      "md": "768px",
+      "lg": "1024px",
+      "xl": "1280px",
+      "2xl": "1536px",
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        "dark-primary": "#161929",
+        "dark-secondary": "#1F2335",
+        "light-primary": "#FFFFFF",
+        "light-secondary": "#F6F6F6",
+      },
+      fontSize: {
+        "xs": "10px",
+        "sm": "12px",
+        "md": "14px",
+        "lg": "16px",
+      },
+      textColor: {
+        "light-primary": "#25293B",
+        "light-secondary": "#C4C8DA",
+        "secondary": "#AFAFAF",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config
