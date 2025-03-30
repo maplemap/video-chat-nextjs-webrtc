ff0aab817a3b71fcdda382ba6ffe84021bc3eba1
Serhii Illarionov <sergio.illarionov@gmail.com>
2025-03-30 19:33:42 +0300

    fix: ts issues

--- app/auth.ts
+++ app/auth.ts
@@ -1,27 +1,15 @@
 import NextAuth from 'next-auth';
 import authConfig from '@/../auth.config';
-import { db } from '@/lib/db';
-import { PrismaAdapter } from '@auth/prisma-adapter';
 
 export const {
   auth,
   signIn,
-  signOut,
   handlers: { GET, POST },
 } = NextAuth({
   pages: {
     signIn: '/auth/sign-in',
     error: '/auth/error',
   },
-  callbacks: {
-    async session({ session, token }) {
-      if (token.sub && session.user) {
-        session.user.id = token.sub;
-      }
-      return session;
-    },
-  },
-  adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
   ...authConfig,
 });
